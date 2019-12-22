5715c78e31f248b54ec8cd299536669f0a9d0956
Evan Plaice <evanplaice@gmail.com>
2019-12-22 13:59:04 -0700

    Refactor

--- src/wc-marquee.js
+++ src/wc-marquee.js
@@ -1,39 +1,5 @@
 /* eslint no-undef: 0 */
-const template = document.createElement('template');
-template.innerHTML = `
-<style>
-.marquee {
-  margin: 0 auto;
-  white-space: nowrap;
-  overflow: hidden;
-  box-sizing: border-box;
-}
-
-.marquee span {
-  display: inline-block;
-  padding-left: 100%;
-  animation: marquee 15s linear infinite;
-}
-
-@keyframes marquee {
-  0% {
-    transform: translate(0, 0);
-  }
-  100% {
-    transform: translate(-100%, 0);
-  }
-}
-</style>
-<p class="marquee" style="width: inherit;"><span><slot></slot></span></p>
-`;
-
 export class WCMarquee extends HTMLElement {
-  constructor () {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(document.importNode(template.content, true));
-  }
-
   static get observedAttributes () {
     return ['party'];
   }
@@ -45,31 +11,77 @@ export class WCMarquee extends HTMLElement {
     }
   }
 
+  get party () { return this.hasAttribute('party'); }
+  set party (value) {
+    const party = Boolean(value);
+    if (party) {
+      this.setAttribute('party', '');
+    } else {
+      this.removeAttribute('party');
+    }
+    this.setParty();
+  }
+
+  constructor () {
+    super();
+    const template = document.createElement('template');
+    template.innerHTML = WCMarquee.default();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.__element = this.shadowRoot.querySelector('.marquee');
+    this.__partifier = null;
+
+  }
+
   async connectedCallback () {
-    // set default width
     this.style.width = (this.style.width) ? this.style.width : '100%';
     this.style.fontFamily = (this.style.fontFamily) ? this.style.fontFamily : 'Comic Sans MS';
-
     this.party = this.hasAttribute('party');
   }
 
-  get party () { return this.getAttribute('party'); }
-  set party (value) {
-    const element = this.shadowRoot.querySelector('.marquee');
-    if (value) {
-      this.partifier = setInterval(function () {
+  setParty() {
+    const party = this.hasAttribute('party');
+    if (party) {
+      this.__partifier = setInterval(function () {
         const r = Math.floor(Math.random() * 255);
         const g = Math.floor(Math.random() * 255);
         const b = Math.floor(Math.random() * 255);
-        element.style.color = `rgb(${r}, ${g}, ${b})`;
+        this.__element.style.color = `rgb(${r}, ${g}, ${b})`;
       }, 400);
       return;
+    } else {
+      if (this.__partifier) {
+        clearInterval(this.partifier);
+      }
     }
+  }
 
-    if (this.partifier) {
-      clearInterval(this.partifier);
-      this.removeAttribute('party');
-    }
+  static default () {
+    return `
+      <style>
+      .marquee {
+        margin: 0 auto;
+        white-space: nowrap;
+        overflow: hidden;
+        box-sizing: border-box;
+      }
+      
+      .marquee span {
+        display: inline-block;
+        padding-left: 100%;
+        animation: marquee 15s linear infinite;
+      }
+      
+      @keyframes marquee {
+        0% {
+          transform: translate(0, 0);
+        }
+        100% {
+          transform: translate(-100%, 0);
+        }
+      }
+      </style>
+      <p class="marquee" style="width: inherit;"><span><slot></slot></span></p>`;
   }
 }
 
