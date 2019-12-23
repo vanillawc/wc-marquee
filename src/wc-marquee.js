e29005889ac8bf7ec606a586a43743be30fb6501
Evan Plaice <evanplaice@gmail.com>
2019-12-22 21:45:21 -0700

    Maintenance

--- src/wc-marquee.js
+++ src/wc-marquee.js
@@ -5,43 +5,48 @@ export class WCMarquee extends HTMLElement {
   }
 
   attributeChangedCallback (name, oldValue, newValue) {
-    switch (name) {
-      case 'party':
-        this.party = this.hasAttribute('party');
+    if (!this.__initialized) { return; }
+    if (oldValue !== newValue) {
+      this[name] = newValue;
     }
   }
 
-  get party () { return this.hasAttribute('party'); }
-  set party (value) {
-    const party = Boolean(value);
-    if (party) {
-      this.setAttribute('party', '');
-    } else {
-      this.removeAttribute('party');
-    }
-    this.setParty();
-  }
+  // get party () { return this.hasAttribute('party'); }
+  // set party (value) {
+  //   const party = this.hasAttribute('party');
+  //   if (party) {
+  //     this.setAttribute('party', '');
+  //   } else {
+  //     this.removeAttribute('party');
+  //   }
+  //   this.setParty();
+  // }
 
   constructor () {
     super();
     const template = document.createElement('template');
-    template.innerHTML = WCMarquee.default();
+    template.innerHTML = WCMarquee.template();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.__element = this.shadowRoot.querySelector('.marquee');
+    this.__initialized = false;
     this.__partifier = null;
   }
 
   async connectedCallback () {
     this.style.width = (this.style.width) ? this.style.width : '100%';
     this.style.fontFamily = (this.style.fontFamily) ? this.style.fontFamily : 'Comic Sans MS';
-    this.party = this.hasAttribute('party');
+    if (this.hasAttribute('party')) {
+      this.setParty();
+    }
+
+    this.__initialized = true;
   }
 
   setParty () {
     const party = this.hasAttribute('party');
     if (party) {
-      this.__partifier = setInterval(function () {
+      this.__partifier = setInterval(() => {
         const r = Math.floor(Math.random() * 255);
         const g = Math.floor(Math.random() * 255);
         const b = Math.floor(Math.random() * 255);
@@ -49,12 +54,12 @@ export class WCMarquee extends HTMLElement {
       }, 400);
     } else {
       if (this.__partifier) {
-        clearInterval(this.partifier);
+        clearInterval(this.__partifier);
       }
     }
   }
 
-  static default () {
+  static template () {
     return `
       <style>
       .marquee {
